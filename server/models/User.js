c77afdd6bc43c2c226d6dc0820751b3377629616
RandyYanish <rtaylorfargo@gmail.com>
2023-08-07 20:04:22 -0700

    User.js added

---
+++ server/models/User.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+const  bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`,
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! Format: xxx-xxx-xxxx`,
+        },
+    },
+    vendor: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+// Set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// Compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
