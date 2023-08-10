263fa6ea6893afa6cd78ae1461d8e5f7ccf650ee
miguel moreno <miguelmoreno142@yahoo.com>
2023-08-09 22:09:44 -0400

    added typeDef

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+// One-to-Many Relationship (User - Business)
+// One-to-Many Relationship (Business - Product)
+// One-to-Many Relationship (User - Cart)
+// One-to-Many Relationship (User - Order)
