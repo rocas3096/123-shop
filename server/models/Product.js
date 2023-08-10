6cd77a72f6c62a52ca86718444e1e14401602a9f
RandyYanish <rtaylorfargo@gmail.com>
2023-08-09 17:24:31 -0700

    Product.js Schema

---
+++ server/models/Product.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = Schema({
+    business_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Business',
+        required: true,
+    },
+    product_name: {
+        type: String,
+        required: true,
+    },
+    product_description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+        min: 0,
+    },
+});
+
+const Product = model('Product', productSchema);
+
+module.exports = Product;
