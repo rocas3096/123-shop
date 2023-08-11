5ae3d0711642d17a5593fb2f3d0c23a29a64b88d
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-08-10 19:37:23 -0600

    create tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#272727",
+        secondary: "#FF2F09",
+        tertiary: "#0085FF",
+        error: "#FF2F09",
+        success: "#10FF70",
+      },
+      backgroundColor: {
+        primary: "#FFFFFF",
+        secondary: "#FF2F09",
+        tertiary: "#FAFAFA",
+        error: "#FF9D8B",
+        success: "#10FF70",
+      },
+    },
   },
   plugins: [],
 };
