23c8cf5fb891d66e01f76d989899485d6427ad93
miguel moreno <miguelmoreno142@yahoo.com>
2023-08-14 22:31:06 -0400

    changed port

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
