dcb1fbd0fdf970149178296a22bc2005e3feb513
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-08-16 17:58:37 -0600

    congifuring for pull

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/123-shop",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
 
